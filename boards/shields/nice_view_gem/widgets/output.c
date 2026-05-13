99027be7445dba1c67d1b53b652399af407f28c7
iThiemann <mail@thiemann.com>
2026-05-13 17:54:14 +0200

    updated zephir 4.1 aso

--- boards/shields/nice_view_gem/widgets/output.c
+++ boards/shields/nice_view_gem/widgets/output.c
@@ -1,4 +1,5 @@
 #include <zephyr/kernel.h>
+
 #include "output.h"
 #include "../assets/custom_fonts.h"
 
@@ -7,72 +8,80 @@ LV_IMG_DECLARE(bt_unbonded);
 LV_IMG_DECLARE(bt);
 LV_IMG_DECLARE(usb);
 
+static void canvas_draw_text_lvgl9(
+    lv_obj_t *canvas,
+    lv_coord_t x,
+    lv_coord_t y,
+    lv_coord_t w,
+    const lv_draw_label_dsc_t *label_dsc,
+    const char *text
+) {
+    lv_layer_t layer;
+    lv_canvas_init_layer(canvas, &layer);
+
+    lv_area_t area = {
+        .x1 = x,
+        .y1 = y,
+        .x2 = x + w - 1,
+        .y2 = y + 20,
+    };
+
+    lv_draw_label_dsc_t dsc = *label_dsc;
+    dsc.text = text;
+
+    lv_draw_label(&layer, &dsc, &area);
+
+    lv_canvas_finish_layer(canvas, &layer);
+}
+
+static void draw_endpoint_text(lv_obj_t *canvas, const char *text) {
+    lv_draw_label_dsc_t label_dsc;
+
+    init_label_dsc(
+        &label_dsc,
+        LVGL_FOREGROUND,
+        &quinquefive_8,
+        LV_TEXT_ALIGN_LEFT
+    );
+
+    canvas_draw_text_lvgl9(
+        canvas,
+        12,
+        140,
+        SCREEN_WIDTH - 8,
+        &label_dsc,
+        text
+    );
+}
+
 #if !IS_ENABLED(CONFIG_ZMK_SPLIT) || IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
 static void draw_usb_connected(lv_obj_t *canvas) {
-    lv_draw_label_dsc_t label_dsc;
-    init_label_dsc(&label_dsc, LVGL_FOREGROUND, &quinquefive_8, LV_TEXT_ALIGN_LEFT);
-    lv_canvas_draw_text(canvas, 12, 140, SCREEN_WIDTH-8, &label_dsc, "USB");
+    draw_endpoint_text(canvas, "USB");
 }
 #endif
 
 static void draw_ble_disconnected(lv_obj_t *canvas) {
-    lv_draw_label_dsc_t label_dsc;
-    init_label_dsc(&label_dsc, LVGL_FOREGROUND, &quinquefive_8, LV_TEXT_ALIGN_LEFT);
-    lv_canvas_draw_text(canvas, 12, 140, SCREEN_WIDTH-8, &label_dsc, "NULL");
+    draw_endpoint_text(canvas, "NULL");
 }
 
 static void draw_ble_connected(lv_obj_t *canvas) {
-    lv_draw_label_dsc_t label_dsc;
-    init_label_dsc(&label_dsc, LVGL_FOREGROUND, &quinquefive_8, LV_TEXT_ALIGN_LEFT);
-    lv_canvas_draw_text(canvas, 12, 140, SCREEN_WIDTH-8, &label_dsc, "BLE");
+    draw_endpoint_text(canvas, "BLE");
 }
 
 void draw_output_status(lv_obj_t *canvas, const struct status_state *state) {
     switch (state->selected_endpoint.transport) {
-        case ZMK_TRANSPORT_USB:
-            draw_usb_connected(canvas);
-            break;
-        case ZMK_TRANSPORT_BLE:
-            draw_ble_connected(canvas);
-            break;
-        default:
-            draw_ble_disconnected(canvas);
-            break;
-    }
-
-    /*
-    lv_draw_label_dsc_t label_dsc;
-    init_label_dsc(&label_dsc, LVGL_FOREGROUND, &lcd_phone, LV_TEXT_ALIGN_LEFT);
-    lv_canvas_draw_text(canvas, 0, 1, 25, &label_dsc, "SIG");
-
-    lv_draw_rect_dsc_t rect_white_dsc;
-    init_rect_dsc(&rect_white_dsc, LVGL_FOREGROUND);
-    lv_canvas_draw_rect(canvas, 43, 0, 24, 15, &rect_white_dsc);
-
 #if !IS_ENABLED(CONFIG_ZMK_SPLIT) || IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
-    switch (state->selected_endpoint.transport) {
     case ZMK_TRANSPORT_USB:
         draw_usb_connected(canvas);
         break;
+#endif
 
     case ZMK_TRANSPORT_BLE:
-        if (state->active_profile_bonded) {
-            if (state->active_profile_connected) {
-                draw_ble_connected(canvas);
-            } else {
-                draw_ble_disconnected(canvas);
-            }
-        } else {
-            draw_ble_unbonded(canvas);
-        }
+        draw_ble_connected(canvas);
+        break;
+
+    default:
+        draw_ble_disconnected(canvas);
         break;
     }
-#else
-    if (state->connected) {
-        draw_ble_connected(canvas);
-    } else {
-        draw_ble_disconnected(canvas);
-    }
-#endif
-*/
 }
